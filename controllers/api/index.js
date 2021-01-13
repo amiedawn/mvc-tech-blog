33fd15e58c3690d427242f48737f7b17ede83653
amiedawn <amie.d.carroll@gmail.com>
2021-01-13 15:46:16 -0700

    complete post-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,10 @@
 const router = require('express').Router();
  
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 // give api endpoints a prefixed name
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
