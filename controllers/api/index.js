5b40d3e8b201a0278dec7ed1f13e8606028b3df1
amiedawn <amie.d.carroll@gmail.com>
2021-01-14 00:41:10 -0700

    add comment info

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
  
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 // give api endpoints a prefixed name
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
