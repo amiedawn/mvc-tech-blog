9761fbfd49e4fb41e67963f1b9366ff1c6786542
amiedawn <amie.d.carroll@gmail.com>
2021-01-14 05:33:56 -0700

    helpers written and tested

---
+++ utils/helpers.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // convert date
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  // pluralize words comment(s) and point(s)
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+    return word;
+  },
+  // shorten a url
+  format_url: (url) => {
+    return url
+      .replace("http://", "")
+      .replace("https://", "")
+      .replace("www.", "")
+      .split("/")[0]
+      .split("?")[0];
+  },
+};
